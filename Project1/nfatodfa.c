d0ebbaafb47524818711c099850472534aedfaab
saikaveti <saileshkaveti@hotmail.com>
2017-09-21 19:08:54 -0400

    Add files via upload

---
+++ Project1/nfatodfa.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include "nfatodfa.h"
+#include "IntSet.h"
+#include "LinkedList.h"
+#include <string.h>
---
+++ Project1/nfatodfa.h
@@ -0,0 +1,6 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <string.h>
+#include "IntSet.h"
+#include "LinkedList.h"
